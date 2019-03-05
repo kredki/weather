2cfbd5b313f2ebc93c41250cc7e72e1d5ff53349
TL <31960565+kredki@users.noreply.github.com>
2019-03-05 15:28:41 +0100

    add passing api key by cmd args

--- src/main/java/weather/server/ServerApplication.java
+++ src/main/java/weather/server/ServerApplication.java
@@ -1,14 +1,34 @@
 package weather.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication implements ApplicationRunner {
+	private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
 	public static void main(String[] args) {
 		// Uncomment line below to use mysql database
 		System.setProperty("spring.profiles.active", "mysql");
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
+		logger.info("NonOptionArgs: {}", args.getNonOptionArgs());
+		logger.info("OptionNames: {}", args.getOptionNames());
+
+		for (String name : args.getOptionNames()){
+			logger.info("arg-" + name + "=" + args.getOptionValues(name));
+		}
+
+		boolean containsOption = args.containsOption("api.key");
+		logger.info("Contains api.key: " + containsOption);
+	}
 }
--- src/main/java/weather/server/service/impl/WeatherServiceImpl.java
+++ src/main/java/weather/server/service/impl/WeatherServiceImpl.java
@@ -1,8 +1,10 @@
 package weather.server.service.impl;
 
+import org.springframework.stereotype.Service;
 import weather.server.service.WeatherService;
 import weather.server.to.WeatherTO;
 
+@Service
 public class WeatherServiceImpl implements WeatherService {
     @Override
     public WeatherTO findCurrentWeatherByCityId(Long id) {
