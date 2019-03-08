b5f39df414fd700fabc2d07ab4eac993cdcf321d
TL <31960565+kredki@users.noreply.github.com>
2019-03-08 14:04:21 +0100

    added weatherTO fields and mapper

--- src/main/java/weather/server/mapper/CurrentWeatherMapper.java
+++ src/main/java/weather/server/mapper/CurrentWeatherMapper.java
@@ -7,6 +7,19 @@ import weather.server.to.CurrentWeatherTO;
 @Component
 public class CurrentWeatherMapper {
     public CurrentWeatherTO toTO(CurrentWeather currentWeatherFromAPI) {
-        return null;
+        if (currentWeatherFromAPI == null) {
+            return null;
+        }
+        CurrentWeatherTO mappedTO = new CurrentWeatherTO();
+        mappedTO.setWindData(currentWeatherFromAPI.getWindData());
+        mappedTO.setWeatherList(currentWeatherFromAPI.getWeatherList());
+        mappedTO.setSnowData(currentWeatherFromAPI.getSnowData());
+        mappedTO.setRainData(currentWeatherFromAPI.getRainData());
+        mappedTO.setDate(currentWeatherFromAPI.getDateTime());
+        mappedTO.setCloudData(currentWeatherFromAPI.getCloudData());
+        mappedTO.setCityId(currentWeatherFromAPI.getCityId());
+        mappedTO.setCityName(currentWeatherFromAPI.getCityName());
+        mappedTO.setMainData(currentWeatherFromAPI.getMainData());
+        return mappedTO;
     }
 }
--- src/main/java/weather/server/to/CurrentWeatherTO.java
+++ src/main/java/weather/server/to/CurrentWeatherTO.java
@@ -1,4 +1,24 @@
 package weather.server.to;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.aksingh.owmjapis.model.param.*;
+
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class CurrentWeatherTO {
+    private Wind windData;
+    private List<Weather> weatherList;
+    private Snow snowData;
+    private Rain rainData;
+    private Date date;
+    private Cloud cloudData;
+    private Integer cityId;
+    private String cityName;
+    private Main mainData;
 }
